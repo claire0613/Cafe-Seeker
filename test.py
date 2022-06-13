5224092ce466e441fbf02677053f1fd5eb0bb452
claire Liang <claire0711@gmail.com>
2022-06-13 18:59:04 +0800

    modify test.py

--- test.py
+++ test.py
@@ -16,22 +16,22 @@ try:
 
     
     if not cache_fetch:
-        print('消失cache')
-        search_count = Rank.query.filter_by(city_id=city_id).order_by(Rank.search_count.desc()).limit(8).all()
-        update_time=datetime.strftime(search_count[0].update_time, "%Y-%m-%d %H:%M")
-        search_list =get_rank(search_count,city_id)
+        print('消失cache',datetime.datetime.now())
+        # search_count = Rank.query.filter_by(city_id=city_id).order_by(Rank.search_count.desc()).limit(8).all()
+        # update_time=datetime.strftime(search_count[0].update_time, "%Y-%m-%d %H:%M")
+        # search_list =get_rank(search_count,city_id)
      
-        cafe_favor = Rank.query.filter_by(city_id=city_id).order_by(Rank.cafe_favor_count.desc()).limit(8).all()
-        favor_list =get_rank(cafe_favor,city_id)
-        cafe_msg = Rank.query.filter_by(city_id=city_id).order_by(Rank.cafe_msg_count.desc()).limit(8).all()
-        msg_list =get_rank(cafe_msg,city_id)
-        cafe_rating = Rank.query.filter_by(city_id=city_id).order_by(Rank.cafe_rating_count.desc()).limit(8).all()
-        rating_list=get_rank(cafe_rating,city_id)
-        data=json.dumps({"data": True, "search_count": search_list, "cafe_favor": favor_list, "cafe_msg": msg_list, "cafe_rating": rating_list,'city_name':city_tw,'update_time':update_time}, cls=DecimalEncoder,ensure_ascii=False)
-        redis_db.setex('fetch',200, data)
+        # cafe_favor = Rank.query.filter_by(city_id=city_id).order_by(Rank.cafe_favor_count.desc()).limit(8).all()
+        # favor_list =get_rank(cafe_favor,city_id)
+        # cafe_msg = Rank.query.filter_by(city_id=city_id).order_by(Rank.cafe_msg_count.desc()).limit(8).all()
+        # msg_list =get_rank(cafe_msg,city_id)
+        # cafe_rating = Rank.query.filter_by(city_id=city_id).order_by(Rank.cafe_rating_count.desc()).limit(8).all()
+        # rating_list=get_rank(cafe_rating,city_id)
+        # data=json.dumps({"data": True, "search_count": search_list, "cafe_favor": favor_list, "cafe_msg": msg_list, "cafe_rating": rating_list,'city_name':city_tw,'update_time':update_time}, cls=DecimalEncoder,ensure_ascii=False)
+        # redis_db.setex('fetch',200, data)
     
-    print('目前有cache',redis_db.get('fetch'))        
-    print(cache_fetch)
+    print('目前有cache',datetime.datetime.now())        
+   
     # else:
     #     search_count = Rank.query.filter_by(city_id=city_id).order_by(Rank.search_count.desc()).limit(8).all()
     #     update_time=datetime.strftime(search_count[0].update_time, "%Y-%m-%d %H:%M")
