99d7bd71ef0bbd6e26d98d14d63249c5fbcfd1b7
claire Liang <claire0711@gmail.com>
2022-06-17 06:46:37 +0800

    modify index loading effect

--- test.py
+++
@@ -1,6 +0,0 @@
-from data.api_helper import city_cafe_filter
-
-
-result = city_cafe_filter(page=0, city='taipei', keyword='', price=0, wifi=0, vacancy=0, drinks=0, quiet=0, comfort=0,
-                                  limited_time='', meal_selling='', rating=0)
-print(result)
