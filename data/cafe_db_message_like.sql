17e75050eaa67eef7c0055e43b8c211c25b2ee1c
claire Liang <claire0711@gmail.com>
2022-06-17 06:13:28 +0800

    upload sql

--- data/cafe_db_message_like.sql
+++ data/cafe_db_message_like.sql
@@ -31,7 +31,7 @@ CREATE TABLE `message_like` (
   KEY `message_like_user_index` (`msg_id`,`user_id`),
   CONSTRAINT `message_like_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
   CONSTRAINT `message_like_ibfk_2` FOREIGN KEY (`msg_id`) REFERENCES `message` (`msg_id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `message_like` (
 
 LOCK TABLES `message_like` WRITE;
 /*!40000 ALTER TABLE `message_like` DISABLE KEYS */;
-INSERT INTO `message_like` VALUES (1,4,4),(2,4,9),(3,4,10),(5,1,11),(4,4,11);
+INSERT INTO `message_like` VALUES (11,10,3),(7,1,4),(1,4,4),(2,4,9),(8,1,10),(9,2,10),(3,4,10),(5,1,11),(4,4,11),(10,1,19);
 /*!40000 ALTER TABLE `message_like` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-13 17:36:31
+-- Dump completed on 2022-06-17  6:12:45
